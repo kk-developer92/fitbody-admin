04c7ea5c05c18dcd134f95e1a41c476dfff5ca1a
ShukurovOzodbek <shukurov.ozodbek.jaloliddinovich@gmail.com>
2023-10-11 16:31:34 +0500

    configs and tailwind installed

---
+++ app.vue
@@ -0,0 +1,11 @@
+<template>
+    <div>
+        <nuxt-layout>
+        </nuxt-layout>
+    </div>
+</template>
+
+
+<script setup lang="ts">
+import '~/assets/css/style.css';
+</script>
---
+++ src/layouts/default.vue
@@ -0,0 +1,13 @@
+<template>
+    <h1>Header</h1>
+    <nuxt-page></nuxt-page>
+    <h1>Footer</h1>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+
+</style>
