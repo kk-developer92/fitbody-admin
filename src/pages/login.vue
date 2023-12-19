b3245db60ba9c4364121dcff131d253318c7a406
ShukurovOzodbek <shukurov.ozodbek.jaloliddinovich@gmail.com>
2023-12-19 20:17:10 +0500

    some changes

--- src/pages/login.vue
+++ src/pages/login.vue
@@ -45,7 +45,7 @@ async function login() {
         return error.value = 'Заполните все поля';
     }
 
-    const response = await axios.post('https://https://sea-turtle-app-27ah7.ondigitalocean.app/authorization', {
+    const response = await axios.post('https://sea-turtle-app-27ah7.ondigitalocean.app/authorization', {
         role: 'admin',
         ...user.value
     });
